20e808ca57cd26facf3e50283436dcf048e8efcd
thomaspdl <pumir.thomas@gmail.com>
2019-06-10 09:35:05 -0400

    Add files via upload

--- FinalGeneration.py
+++ FinalGeneration.py
@@ -13,15 +13,15 @@ from sklearn import linear_model
 # dimension of the problem
 #####################################
 p = 3 #dimension
-k = 250 #number of points
+k = 100 #number of points
 
 
 #####################################
 # hyperparameters
 #####################################
 Thresh = 0.95 #recovery threshold
-OrdinatesOrigin = 6 #Ordinate at origin for Gram recovery
-alpha = 3.5/5. #portion of the graph where oracle recovery is displayed
+OrdinatesOrigin = 9 #Ordinate at origin for Gram recovery
+alpha = 4.2/5. #portion of the graph where oracle recovery is displayed
 
 #####################################
 # generate cloud
@@ -29,7 +29,7 @@ alpha = 3.5/5. #portion of the graph where oracle recovery is displayed
 X = np.zeros((p,k))
 for l in range(1,k):
     v = np.random.randn(p)
-    v = v/LA.norm(v)
+    #v = v/LA.norm(v)
     X[:,l] = v
 
 X = X/LA.norm(X,'fro')
@@ -77,6 +77,8 @@ for i in range(len(sigma)):
         MatErrOracle[i][j] = int(k*p*(s**2)/n >= Thresh)
 
 
+
+
 #####################################
 # Get the slope for Gram Recovery
 #####################################
@@ -98,6 +100,7 @@ for c in range(len(N)):
 
 xG = np.asarray([u for u in range(len(N))]).reshape(-1, 1)
 yG = np.asarray(FG).reshape(-1, 1)
+
 
 #fit the least squares and get the slope
 reg_Gram   = linear_model.LinearRegression()
@@ -127,10 +130,21 @@ for c in range(len(N)):
 xO = np.asarray([u for u in range(len(N) - 8)]).reshape(-1, 1)
 yO = np.asarray([FO[u] for u in range(len(FO)-8)]).reshape(-1, 1)
 
-#fit the least squares and get the slope
-reg_Oracle = linear_model.LinearRegression()
-reg_Oracle.fit(xO, yO)
-slopeO = reg_Oracle.coef_[0][0]
+
+
+# #fit the least squares and get the slope
+# # sklearn version
+# reg_Oracle = linear_model.LinearRegression()
+# reg_Oracle.fit(xO, yO)
+# slopeO = reg_Oracle.coef_[0][0]
+
+#other method: pick two points and get slope in close form
+n0 = xO[0]
+n1 = xO[-1]
+sig0 = yO[0]
+sig1 = yO[-1]
+
+slopeO = float(sig1 - sig0)/float(n1 - n0)
 
 
 
@@ -147,17 +161,27 @@ y_GR = [FG[OrdinatesOrigin] + 0.5*slopeO*u for u in range(int(len(N)))]
 #####################################
 # Actual plot
 #####################################
-fig, ax = plt.subplots(figsize = (12,8))
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import rcParams
+plt.rcParams["font.family"] = "Times New Roman"
+#rcParams['font.serif'] = ['Roman']
+
+fig, ax = plt.subplots(figsize = (22, 14))
 im = plt.imshow(MatErr, interpolation = 'none', cmap='binary',origin='lower')
-plt.plot(x_GR,y_GR,'r',label = '$\sigma = c\cdot\sqrt{N}$')
-plt.plot(x_OR,y_OR,'b',label = '$\sigma = C \cdot N^{1/4}$')
-plt.xticks(x_labels,[str(u) for u in Nxlabels],fontsize=18)
-plt.yticks(y_labels,['$2^{' + str(round(u, 1)) + '}$' for u in Sylabels],fontsize=18)
+plt.plot(x_GR,y_GR,'b',label = '$\sigma = c\cdot N^{1/4}$',linewidth=2)
+plt.plot(x_OR,y_OR,'r',label = '$\sigma = C \cdot \sqrt{N}$',linewidth=2)
+plt.xticks(x_labels,[str(u) for u in Nxlabels],fontsize=25)
+plt.yticks(y_labels,['$2^{' + str(round(u, 1)) + '}$' for u in Sylabels],fontsize=25)
 # plt.locator_params(axis = 'x',nbins=len(Nplot))
 # plt.locator_params(axis = 'y',nbins=len(sigmaplot))
-plt.title('Estimation Error ',fontsize=30)
-plt.xlabel('Number of observations N',fontsize=24)
-plt.ylabel('Noise level $\sigma$',fontsize=24)
-plt.legend(loc='lower right',prop={'size': 20},frameon=False)
-#plt.colorbar()
+plt.title('Estimation Error ',fontsize=40)
+plt.xlabel('Number of observations $N$',fontsize=36)
+plt.ylabel('Noise level $\sigma$',fontsize=36)
+plt.legend(loc='lower right',prop={'size': 30},frameon=False)
+divider = make_axes_locatable(ax)
+cax = divider.append_axes('right', size='5%', pad=0.05)
+cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+cbar.ax.tick_params(labelsize=25)
+plt.savefig('/Users/thomaspumir/Dropbox/PumirBoumal/GeneralizedProcrustesProblem/writeup/ArXiv/DonohoPlotsFinal.png')
 plt.show()
