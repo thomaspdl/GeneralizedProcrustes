833f28c89815179cddc31cda46922b3ef20f96eb
thomaspdl <pumir.thomas@gmail.com>
2019-06-10 09:36:04 -0400

    Add files via upload

--- PlotResults.py
+++ PlotResults.py
@@ -13,15 +13,15 @@ from tempfile import TemporaryFile
 d = 3
 k = 250
 sigma = 1
-Ns = [5000,6000,7000,8000,9000,10000]
+Ns = [2**u for u in range(1,15)]
 
 ###########################
 #save the file
 
-Ns = np.loadtxt('Ns250.txt')
-Sigma2Mean = np.loadtxt('Sigma2Mean250.txt')
-Sigma2Var = np.loadtxt('Sigma2Var250.txt')
-Sigma2VarPred = np.loadtxt('Sigma2VarPred250.txt')
+Ns = np.loadtxt('Ns.txt')
+Sigma2Mean = np.loadtxt('Sigma2Mean.txt')
+Sigma2Var = np.loadtxt('Sigma2Var.txt')
+Sigma2VarPred = np.loadtxt('Sigma2VarPred.txt')
 
 ErrMean = np.subtract(np.ones(Ns.size), np.divide(Sigma2Mean,sigma**2))
 ErrVar  = np.subtract(np.divide(Sigma2VarPred,sigma**2),np.divide(Sigma2Var,sigma**2))
@@ -41,46 +41,76 @@ def myticks(x,pos):
 
 x = np.arange(0, 1, .01)
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import rcParams
+plt.rcParams["font.family"] = "Times New Roman"
+
 fig,ax = plt.subplots(figsize=(22, 14))
-ax.plot(Ns,ErrMean,'ro',markersize=16)
-#ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-#plt.ylim(0,1.5*max(ErrMean))
-#plt.yticks()
-#ax.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-#ax.xaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-plt.xticks(fontsize=40)
-plt.yticks(fontsize=30)
-plt.ylabel('$\sigma^2 - \hat{\sigma}_{N}^{2}$', fontsize=44)
-plt.xlabel('Number of Observations N', fontsize=48)
-plt.title(' Bias of the estimator', fontsize=56)
-plt.savefig('/Users/thomaspumir/Dropbox/PumirBoumal/GeneralizedProcrustesProblem/writeup/FinalPaper/BiasEstimatorSigma.png')
+ax.loglog(Ns,ErrMean,'ro',markersize=16)
+plt.xticks(fontsize=25)
+plt.yticks(fontsize=25)
+plt.xlabel('Number of observations N', fontsize=36)
+plt.ylabel('Relative Error',fontsize=36)
+plt.title(' Relative Error of the estimator of $\sigma^2$', fontsize=40)
+plt.savefig('/Users/thomaspumir/Dropbox/PumirBoumal/GeneralizedProcrustesProblem/writeup/ArXiv/BiasEstimatorSigma.png')
 plt.show()
 
 
 
-fig,ax = plt.subplots(figsize=(22, 14))
-#plt.subplot(121)
-ax.plot(Ns,np.divide(Sigma2Var,sigma**2),'ro',label="Empirical Variance",markersize=16)
-ax.plot(Ns,np.divide(Sigma2VarPred,sigma**2),'b',label="Predicted Variance")
-#ax.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-#plt.yticks(Sigma2Var,[str(round(u*(10**6), 1)) for u in Sigma2Var],fontsize=12)
-#plt.ylim(0,1.25*max(Sigma2Var))
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-plt.xticks(fontsize=40)
-plt.yticks(fontsize=30)
-plt.xlabel('Number of Observations N', fontsize=48)
-plt.ylabel('Relative Variance', fontsize=44)
-plt.title('Variance Of the Estimator', fontsize=56)
-plt.legend(prop={'size': 36})
-# plt.subplot(122)
-# plt.plot(Ns,ErrVar,'r',label="Variance")
-# #plt.plot(Ns,np.divide(np.multiply(Sigma2VarPred,Ns),sigma**2),'b',label="Predicted Variance")
-# #plt.ylim(100,160)
-# plt.xlabel('Number of Samples')
-# plt.ylabel('Relative Variance Estimation Error')
-# plt.legend()
-plt.savefig('/Users/thomaspumir/Dropbox/PumirBoumal/GeneralizedProcrustesProblem/writeup/FinalPaper/VarianceEstimatorSigma.png')
-plt.show()
+# ##################################
+# ##################################
+#
+#
+#
+# fig, ax = plt.subplots(figsize = (22, 14))
+#
+# plt.xticks(x_labels,[str(u) for u in Nxlabels],fontsize=25)
+# plt.yticks(y_labels,['$2^{' + str(round(u, 1)) + '}$' for u in Sylabels],fontsize=25)
+#
+# plt.title('Estimation Error ',fontsize=40)
+# plt.xlabel('Number of observations $N$',fontsize=36)
+# plt.ylabel('Noise level $\sigma$',fontsize=36)
+# plt.legend(loc='lower right',prop={'size': 30},frameon=False)
+# divider = make_axes_locatable(ax)
+# cax = divider.append_axes('right', size='5%', pad=0.05)
+# cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+# cbar.ax.tick_params(labelsize=25)
+#
+#
+#
+# ##################################
+# ##################################
+
+
+
+
+
+
+
+
+# fig,ax = plt.subplots(figsize=(22, 14))
+# #plt.subplot(121)
+# ax.plot(Ns,np.divide(Sigma2Var,sigma**2),'ro',label="Empirical variance",markersize=16)
+# ax.plot(Ns,np.divide(Sigma2VarPred,sigma**2),'b',label="Predicted variance")
+# #ax.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
+# #plt.yticks(Sigma2Var,[str(round(u*(10**6), 1)) for u in Sigma2Var],fontsize=12)
+# #plt.ylim(0,1.25*max(Sigma2Var))
+# ax.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
+# plt.xticks(fontsize=25)
+# plt.yticks(fontsize=25)
+# plt.xlabel('Number of observations N', fontsize=40)
+# plt.ylabel('Relative variance', fontsize=40)
+# plt.title('Variance of the estimator', fontsize=48)
+# plt.legend(prop={'size': 30})
+# # plt.subplot(122)
+# # plt.plot(Ns,ErrVar,'r',label="Variance")
+# # #plt.plot(Ns,np.divide(np.multiply(Sigma2VarPred,Ns),sigma**2),'b',label="Predicted Variance")
+# # #plt.ylim(100,160)
+# # plt.xlabel('Number of Samples')
+# # plt.ylabel('Relative Variance Estimation Error')
+# # plt.legend()
+# plt.savefig('/Users/thomaspumir/Dropbox/PumirBoumal/GeneralizedProcrustesProblem/writeup/ArXiv/VarianceEstimatorSigma.png')
+# plt.show()
 
 
 # plt.figure(figsize=(5,5))
